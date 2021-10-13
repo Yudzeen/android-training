3355c546d7180b5de2d0b05e87e703121d8efd5b
Eugene Javiñas <eugenejavinas@gmail.com>
2021-10-13 17:00:12 +0800

    Modularize MultipleLauncherApp

---
+++ MutipleLauncherApp/src/main/java/ebj/yujinkun/mutiplelauncherapp/Main2Activity.java
@@ -0,0 +1,21 @@
+package ebj.yujinkun.mutiplelauncherapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
