99476f45ffa00eca7f5bd6c063daf307b272ffc6
Eugene Javiñas <eugenejavinas@gmail.com>
2021-10-13 17:07:41 +0800

    Modularize NotificationScheduler

---
+++ NotificationScheduler/src/main/java/ebj/yujinkun/notificationscheduler/MainActivity.java
@@ -0,0 +1,119 @@
+package ebj.yujinkun.notificationscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int JOB_ID = 0;
+
+    private JobScheduler jobScheduler;
+    private Switch deviceIdleSwitch;
+    private Switch deviceChargingSwitch;
+    private SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        deviceIdleSwitch = findViewById(R.id.idleSwitch);
+        deviceChargingSwitch = findViewById(R.id.chargingSwitch);
+        seekBar = findViewById(R.id.seekBar);
+
+        final TextView seekBarProgress = findViewById(R.id.seekBarProgress);
+
+        jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress > 0) {
+                    seekBarProgress.setText(getString(R.string.seconds, progress));
+                } else {
+                    seekBarProgress.setText(R.string.not_set);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
+    }
+
+    public void scheduleJob(View view) {
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+
+        int selectedNetworkId = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        int seekBarInteger = seekBar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
+
+        switch (selectedNetworkId) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption  = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+
+        final boolean requiresDeviceIdle = deviceIdleSwitch.isChecked();
+        final boolean requiresCharging = deviceChargingSwitch.isChecked();
+
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
+                .setRequiredNetworkType(selectedNetworkOption)
+                .setRequiresDeviceIdle(requiresDeviceIdle)
+                .setRequiresCharging(requiresCharging);
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
+        boolean constraintSet = selectedNetworkOption
+                != JobInfo.NETWORK_TYPE_NONE
+                || requiresDeviceIdle
+                || requiresCharging
+                || seekBarSet;
+
+        if (constraintSet) {
+            JobInfo jobInfo = builder.build();
+            int result = jobScheduler.schedule(jobInfo);
+            if (result == JobScheduler.RESULT_SUCCESS) {
+                Toast.makeText(this, R.string.job_scheduled, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.failed_to_schedule_job, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.no_constraint_toast, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void cancelJobs(View view) {
+        if (jobScheduler != null) {
+            jobScheduler.cancelAll();
+            jobScheduler = null;
+            Toast.makeText(this, R.string.jobs_canceled, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
