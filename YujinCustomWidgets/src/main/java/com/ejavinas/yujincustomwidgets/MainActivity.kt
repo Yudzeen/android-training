26f873098d86acb1a32a9841bf14421903e3780f
Eugene Javiñas <eugenejavinas@gmail.com>
2022-08-01 19:59:30 +0800

    Add custom widgets

---
+++ YujinCustomWidgets/src/main/java/com/ejavinas/yujincustomwidgets/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ejavinas.yujincustomwidgets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ YujinCustomWidgets/src/main/java/com/ejavinas/yujincustomwidgets/MainApplication.kt
@@ -0,0 +1,12 @@
+package com.ejavinas.yujincustomwidgets
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
